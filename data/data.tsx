9ad68334caab25fdd7bff52f8a776bb4288b8ea7
Fayzullo07 <jorayevfayzillo77@gmail.com>
2024-05-03 10:33:21 +0500

    any 1

--- data/data.tsx
+++ data/data.tsx
@@ -8,7 +8,7 @@ export const navbar = [
     { icon: <NewspaperIcon />, name: "Loyihalar", slug: "/projects" },
     { icon: <HandshakeIcon />, name: "Hamkorlar", slug: "/partners" },
     { icon: <CalendarCheckIcon />, name: "Tadbirlar", slug: "/events" },
-    { icon: <NewspaperIcon />, name: "Tashabbuskorlar", slug: "/initiators" },
+    // { icon: <NewspaperIcon />, name: "Tashabbuskorlar", slug: "/initiators" },
     { icon: <NewspaperIcon />, name: "Xizmatlar", slug: "/services" },
     // { icon: <NewspaperIcon />, name: "Aql Markazi", slug: "/center_mind" },
     // { icon: <NewspaperIcon />, name: "Ilmiy etika", slug: "/ilmiy_etika" },
